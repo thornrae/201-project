13526e8c9726135b997a7ead0b1e02d8e8672d13
arnone215 <tarnone21589@gmail.com>
2020-11-22 17:41:34 -0800

    results.css created, js/html updated

--- results.js
+++ results.js
@@ -47,27 +47,39 @@ console.log('player data: ' + playerDataArray);
 
 
 // --------------------------------------------------------
-// var parentElement = document.getElementById('result-table');
-// var outPut = parseInt(localStorage.getItem('playerScore'));
+var parentElement = document.getElementById('result-table');
+var outPut = parseInt(localStorage.getItem('playerScore'));
 
 // var users = [{ name: 'Taylor', score: 3 }, { name: 'Thomas', score: 4 }, { name: 'Matthew', score: 6 }, { name: 'Dawit', score: 9 }];
-// for (var i = 0; i < users.length; i++) {
-//   if (users[i].score < outPut) {
-//     var scoreRow = document.createElement('tr');
-//     var scoreHead = document.createElement('th');
-//     scoreHead.textContent = localStorage.getItem('userName');
-//     var tableData = document.createElement('td');
-//     tableData.textContent = localStorage.getItem('playerScore');
-//     scoreRow.appendChild(scoreHead);
-//     scoreRow.appendChild(tableData);
-//     parentElement.appendChild(scoreRow);
-//   }
-//   var scoreRowOne = document.createElement('tr');
-//   var scoreHeadOne = document.createElement('th');
-//   scoreHeadOne.textContent = users[i].name;
-//   var tableDataOne = document.createElement('td');
-//   tableDataOne.textContent = users[i].score;
-//   scoreRowOne.appendChild(scoreHeadOne);
-//   scoreRowOne.appendChild(tableDataOne);
-//   parentElement.appendChild(scoreRowOne);
-// }
+
+var users = [
+  { name: 'Taylor', score: 63 },
+  { name: 'Thomas', score: 74 },
+  { name: 'Matthew',score: 96 },
+  { name: 'Dawit', score:  9 },
+];
+//sort by value
+function descending (a,b){
+  return b.score - a.score;
+}
+console.log(users.sort(descending));
+for (var i = 0; i < users.length; i++) {
+  if (users[i].score < outPut) {
+    var scoreRow = document.createElement('tr');
+    var scoreHead = document.createElement('th');
+    scoreHead.textContent = localStorage.getItem('playerName');
+    var tableData = document.createElement('td');
+    tableData.textContent = localStorage.getItem('playerScore');
+    scoreRow.appendChild(scoreHead);
+    scoreRow.appendChild(tableData);
+    parentElement.appendChild(scoreRow);
+  }
+  var scoreRowOne = document.createElement('tr');
+  var scoreHeadOne = document.createElement('th');
+  scoreHeadOne.textContent = users[i].name;
+  var tableDataOne = document.createElement('td');
+  tableDataOne.textContent = users[i].score;
+  scoreRowOne.appendChild(scoreHeadOne);
+  scoreRowOne.appendChild(tableDataOne);
+  parentElement.appendChild(scoreRowOne);
+}
